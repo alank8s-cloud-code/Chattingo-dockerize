721acbba809b519ea47c5926f25df2d65fc2bcc0
alank8s cloud <alank8s.cloud@gmail.com>
2026-09-08 13:32:07 +0000

    update the realtime issue

--- backend/src/main/java/com/chattingo/Controller/RealTimeChat.java
+++ backend/src/main/java/com/chattingo/Controller/RealTimeChat.java
@@ -1,21 +1,26 @@
 package com.chattingo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 import com.chattingo.Model.Message;
 
+@Controller
 public class RealTimeChat {
 
+    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")
-    @SendTo("/group/public")
-    public Message recieveMessage(@Payload Message message) {
-        simpMessagingTemplate.convertAndSend("/group" + message.getChat().getId().toString(), message);
-        return message;
+    public void recieveMessage(@Payload Message message) {
+        Integer chatId = message.getChat().getId();
+        boolean isGroup = message.getChat().isGroup();
+
+        String destination = (isGroup ? "/group/" : "/user/") + chatId;
+        simpMessagingTemplate.convertAndSend(destination, message);
     }
 
 }
